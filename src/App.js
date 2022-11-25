07a2bd26cd56cc15d82ae3bc8b2c03ef8eba2c90
ketanmalik <ketanmalik@gmail.com>
2022-11-25 13:10:24 +0530

    Adding search bar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import logo from "./logo.svg";
+import user from "./user.png";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -7,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // const naiveComponent = React.createElement("div", { className: "title" }, [
 //     React.createElement("h1", { id: "heading-1" }, "Hello, world"),
 //     React.createElement("h2", { id: "heading-2" }, "This is a React component"),
-//     React.createElement("h3", { id: "heading-3" }, "It is created in the naive way using 'React.createElement'")
+//     React.createElement("h3", { className: "heading-3" }, "It is created in the naive way using 'React.createElement'")
 // ]);
 // root.render(naiveComponent);
 
@@ -16,19 +18,54 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //     <div>
 //         <h1 id="heading-1">Hello, World</h1>
 //         <h2 id="heading-2">This is a React component</h2>
-//         <h3 id="heading-3">It is created using JSX</h3>
+//         <h3 className="heading-3">It is created using JSX</h3>
 //     </div>
 // )
 // root.render(jsxComponent);
 
 // 3. Creating a functional component
-const FunctionalComponent = () => {
-    return (
-        <div>
-            <h1 id="heading-1">Hello, World</h1>
-            <h2 id="heading-2">This is a React component</h2>
-            <h3 id="heading-3">It's a functional component</h3>
-        </div>
-    )
-}
-root.render(<FunctionalComponent />);
+// const FunctionalComponent = () => {
+//     return (
+//         <div>
+//             <h1 id="heading-1">Hello, World</h1>
+//             <h2 id="heading-2">This is a React component</h2>
+//             <h3 className="heading-3">It's a functional component</h3>
+//         </div>
+//     )
+// }
+// root.render(<FunctionalComponent />);
+
+// 4. Creating a JSX TitleComponent, Functional HeaderComponent, and SearchComponent
+const jsxTitleComponent = (
+  <h3 className="heading-3">This is a JSX Component</h3>
+);
+const FunctionalTitleComponent = () => (
+  <h3 className="heading-3">This is a Functional Component</h3>
+);
+
+const SearchComponent = () => {
+  return (
+    <div className="search">
+      <img className="react-logo" src={logo} alt="logo" />
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search anything..."
+        autoFocus
+      />
+      <img className="user-logo" src={user} alt="user" />
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <h1>React Bootcamp 2022</h1>
+      <SearchComponent />
+      {jsxTitleComponent}
+      <FunctionalTitleComponent />
+    </div>
+  );
+};
+root.render(<HeaderComponent />);
